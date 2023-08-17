41c36d2c6668eb03b2a74e1eabf80610bb8cc6da
Nichole <nicholemcgrew@gmail.com>
2023-08-17 14:31:37 -0500

    completed Methods section

--- main.js
+++ main.js
@@ -436,3 +436,44 @@ function randomPoint(radius) {
 
 let string = JSON.stringify({squirrel: false,
     events: ["weekend"]});
+
+
+//     Separating interface from implementation is a great idea. It is usually called encapsulation.
+
+// Methods
+
+let rabbit = {};
+rabbit.speak = function(line) {
+    // console.log(`The rabbit says '${line}'`);
+};
+
+rabbit.speak("I'm alive.");
+
+function speak(line) {
+    // console.log(`The ${this.type} rabbit says '${line}'`);
+}
+let whiteRabbit = {type: "white", speak};
+let hungryRabbit = {type: "hungry", speak};
+
+whiteRabbit.speak("Oh my ears and whiskers, " + 
+                   "how late it's getting!");
+hungryRabbit.speak("I could use a carrot right now.")
+
+speak.call(hungryRabbit, "Burp!")
+
+function normalize() {
+    // console.log(this.coords.map(n => n / this.length));
+}
+normalize.call({coords: [0, 2, 3], length: 5})
+
+let protoRabbit = {
+    speak(line) {
+        // console.log(`The ${this.type} rabbit says "${line}"`);
+    }
+}
+let killerRabit = Object.create(protoRabbit);
+killerRabit.type = "killer";
+killerRabit.speak("SKREEE!")
+
+// Classes
+
