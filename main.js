e7f8700f3d8846db7b515cb99ed1daabb899dc36
Nichole <nicholemcgrew@gmail.com>
2023-08-18 09:19:40 -0500

    finished ch06 reading

--- main.js
+++ main.js
@@ -477,3 +477,222 @@ killerRabit.speak("SKREEE!")
 
 // Classes
 
+function makeRabbit(type) {
+    let rabbit = Object.create(protoRabbit);
+    rabbit.type = type;
+    return rabbit;
+}
+
+function Rabbit(type) {
+    this.type = type;
+}
+Rabbit.prototype.speak = function(line) {
+    // console.log(`The ${this.type} rabbit says '${line};`);
+};
+
+let weirdRabbit = new Rabbit('weird');
+
+// Class notation
+
+class Rabbit2 {
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line) {
+        console.log(`The ${this.type} rabbit2 says '${line}'`)
+    }
+}
+
+let killerRabbit = new Rabbit('killer');
+let blackRabbit = new Rabbit('black');
+
+killerRabbit.speak('hi')
+
+
+let object = new class { getWord() { return "hello"; } };
+// console.log(object.getWord());
+
+// Overriding derived properties
+
+Rabbit.prototype.teeth = 'small';
+// console.log(killerRabbit.teeth)
+killerRabbit.teeth = 'long, sharp, and bloody';
+// console.log(killerRabbit.teeth);
+// console.log(blackRabbit.teeth);
+// console.log(Rabbit.prototype.teeth);
+
+// Maps
+
+// let ages = {
+//   Boris: 39,
+//   Liang: 22,
+//   Júlia: 62
+// };
+
+// console.log(`Júlia is ${ages["Júlia"]}`);
+// console.log("Is Jack's age known?", "Jack" in ages);
+// console.log("Is toString's age known?", "toString" in ages);
+
+let ages = new Map();
+ages.set('Boris', 39);
+ages.set('Liang', 22);
+ages.set('Júlia', 62);
+
+// console.log(`Júlia is ${ages.get("Júlia")}`);
+// console.log("Is Jack's age known?", ages.has("Jack"));
+// console.log(ages.has("toString"));
+
+// Polymorphism
+
+Rabbit.prototype.toString = function() {
+    return `a ${this.type} rabbit`;
+}
+
+// console.log(String(blackRabbit))
+
+// Symbols
+
+let sym = Symbol('name');
+// console.log(sym == Symbol('name'))
+
+Rabbit.prototype[sym] = 55;
+// console.log(blackRabbit[sym])
+
+const toStringSymbol = Symbol('toString');
+Array.prototype[toStringSymbol] = function() {
+    return `${this.length} cm of blue yarn`;
+};
+
+// console.log([1, 2].toString());
+// console.log([1, 2][toStringSymbol]());
+
+let stringObject = {
+    [toStringSymbol]() { return 'a jute rope'; }
+};
+
+// console.log(stringObject[toStringSymbol]());
+
+// The iterator interface
+
+
+let okIterator = 'OK'[Symbol.iterator]();
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+
+ class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+
+    get(x, y) {
+        return this.content[y * this.width + x];
+    }
+    set(x, y, value) {
+        this.content[y * this.width + x] = value;
+    }
+ }
+
+ class MatrixInterator {
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        if (this.y == this.matrix.height) return {done: true};
+
+        let value = {x: this.x, 
+                      y: this.y,
+                    value: this.matrix.get(this.x, this.y)};
+        this.x++;
+        if (this.x == this.matrix.width) {
+            this.x = 0;
+            this.y++;
+        }
+        return {value, done: false};
+    }
+}
+Matrix.prototype[Symbol.iterator] = function() {
+   return new MatrixInterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+for (let {x, y, value} of matrix) {
+   console.log(x, y, value);
+}
+
+
+// Getters, setters, and statics
+
+let varyingSize = {
+    get size() {
+        return Math.floor(Math.random() * 100);
+    }
+};
+
+console.log(varyingSize.size)
+console.log(varyingSize.size)
+
+class Temperature {
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32;
+    }
+    set fahrenheit(value) {
+        this.celsius = (value - 32) /1.8;
+    }
+    
+    static fromFahrenheit(value) {
+        return new Temperature((value - 32) / 1.8);
+    }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
+temp.fahrenheit = 86;
+console.log(temp.celsius);
+
+// Inheritance
+// The new class inherits properties and behavior from the old class.
+
+class SymmetricMatrix extends Matrix {
+    constructor(size, element = (x, y) => undefined) {
+        super(size, size, (x, y) => {
+            if (x < y) return element(y, x);
+            else return element(x, y);
+        });
+    }
+
+    set(x, y, value) {
+        super.set(x, y, value);
+        if (x != y) {
+            super.set(y, x, value);
+        }
+    }
+}
+
+matrix = new SymmetricMatrix(5, (x, y) => `${x}, ${y}`);
+console.log(matrix.get(2, 3));
+
+// The instanceof operator
+console.log(
+    new SymmetricMatrix(2) instanceof SymmetricMatrix);
+  // → true
+  console.log(new SymmetricMatrix(2) instanceof Matrix);
+  // → true
+  console.log(new Matrix(2, 2) instanceof SymmetricMatrix);
+  // → false
+  console.log([1] instanceof Array);
+  // → true
